31f63470b645ba990ac7fad61dc240b6180a7d6a
biboon <r.libaert@gmail.com>
2017-05-27 17:34:11 +0200

    Better colors but nwère c nwère

--- log.h
+++ log.h
@@ -23,13 +23,13 @@
 #define LOG_LEVEL 2
 #endif
 
-#define LOG_PREFIX_ALL   "[" "\x1B[90m" "  all" "\x1B[0m" "] "
-#define LOG_PREFIX_TRACE "[" "\x1B[97m" "trace" "\x1B[0m" "] "
-#define LOG_PREFIX_DEBUG "[" "\x1B[96m" "debug" "\x1B[0m" "] "
-#define LOG_PREFIX_INFO  "[" "\x1B[92m" " info" "\x1B[0m" "] "
-#define LOG_PREFIX_WARN  "[" "\x1B[93m" " warn" "\x1B[0m" "] "
-#define LOG_PREFIX_ERROR "[" "\x1B[91m" "error" "\x1B[0m" "] "
-#define LOG_PREFIX_FATAL "[" "\x1B[95m" "fatal" "\x1B[0m" "] "
+#define LOG_PREFIX_ALL   "[" "\x1B[30m" "  all" "\x1B[0m" "] "
+#define LOG_PREFIX_TRACE "[" "\x1B[37m" "trace" "\x1B[0m" "] "
+#define LOG_PREFIX_DEBUG "[" "\x1B[36m" "debug" "\x1B[0m" "] "
+#define LOG_PREFIX_INFO  "[" "\x1B[32m" " info" "\x1B[0m" "] "
+#define LOG_PREFIX_WARN  "[" "\x1B[33m" " warn" "\x1B[0m" "] "
+#define LOG_PREFIX_ERROR "[" "\x1B[31m" "error" "\x1B[0m" "] "
+#define LOG_PREFIX_FATAL "[" "\x1B[35m" "fatal" "\x1B[0m" "] "
 
 #if LOG_LEVEL <= 0
 #define log_all(fmt, ...) \
